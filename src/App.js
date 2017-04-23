5d46ec9a882307368c0dcd34c2b3b924659a11a2
mlukasch <mlukasch@gmail.com>
2017-04-23 23:19:20 +0200

    init2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default () => (
-  <View>
-    <Text>HAlloossssssssssossoo</Text>
+  <View style={styles.viewStyle}>
+    <Text style={styles.textStyle}>HAlloossssssssssossoo</Text>
   </View>
 );
+
+const styles = {
+  textStyle: {
+    borderWidth: 1,
+    borderColor: 'black',
+    fontSize: 24,
+  },
+  viewStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+};
+console.log("stylesssss")
